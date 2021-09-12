2853cfdae21db8af71f5706fed65b3aa9aa7d84c
rexim <reximkut@gmail.com>
2021-09-12 18:43:19 +0700

    Rename cmd_echoed -> cmd_call_echoed

--- porth.py
+++ porth.py
@@ -307,7 +307,7 @@ def lex_file(file_path):
 def load_program_from_file(file_path):
     return crossreference_blocks([parse_token_as_op(token) for token in lex_file(file_path)])
 
-def cmd_echoed(cmd):
+def cmd_call_echoed(cmd):
     print("[CMD] %s" % " ".join(map(shlex.quote, cmd)))
     subprocess.call(cmd)
 
@@ -385,10 +385,10 @@ if __name__ == '__main__' and '__file__' in globals():
         print("[INFO] Generating %s" % (basepath + ".asm"))
         program = load_program_from_file(program_path);
         compile_program(program, basepath + ".asm")
-        cmd_echoed(["nasm", "-felf64", basepath + ".asm"])
-        cmd_echoed(["ld", "-o", basepath, basepath + ".o"])
+        cmd_call_echoed(["nasm", "-felf64", basepath + ".asm"])
+        cmd_call_echoed(["ld", "-o", basepath, basepath + ".o"])
         if run:
-            cmd_echoed([basepath])
+            cmd_call_echoed([basepath])
     elif subcommand == "help":
         usage(compiler_name)
         exit(0)
