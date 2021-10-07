e4e4f5d9f76a6667108420ce1a97af943041207f
rexim <reximkut@gmail.com>
2021-10-07 21:27:10 +0700

    Cleanup magic constants

--- porth.py
+++ porth.py
@@ -14,6 +14,10 @@ import traceback
 PORTH_EXT = '.porth'
 DEFAULT_EXPANSION_LIMIT=1000
 EXPANSION_DIAGNOSTIC_LIMIT=10
+MEM_CAPACITY  = 640_000 # should be enough for everyone
+SIM_NULL_POINTER_PADDING = 1 # just a little bit of a padding at the beginning of the memory to make 0 an invalid address
+SIM_STR_CAPACITY  = 640_000
+SIM_ARGV_CAPACITY = 640_000
 
 debug=False
 
@@ -102,10 +106,6 @@ class Op:
 
 Program=List[Op]
 
-NULL_POINTER_PADDING = 1 # just a little bit of a padding at the beginning of the memory to make 0 an invalid address
-STR_CAPACITY  = 640_000 # should be enough for everyone
-MEM_CAPACITY  = 640_000
-ARGV_CAPACITY = 640_000
 
 def get_cstr_from_mem(mem: bytearray, ptr: int) -> bytes:
     end = ptr
@@ -119,16 +119,16 @@ def simulate_little_endian_linux(program: Program, argv: List[str]):
     ENOENT=2
 
     stack: List[int] = []
-    mem = bytearray(NULL_POINTER_PADDING + STR_CAPACITY + ARGV_CAPACITY + MEM_CAPACITY)
+    mem = bytearray(SIM_NULL_POINTER_PADDING + SIM_STR_CAPACITY + SIM_ARGV_CAPACITY + MEM_CAPACITY)
 
-    str_buf_ptr  = NULL_POINTER_PADDING
+    str_buf_ptr  = SIM_NULL_POINTER_PADDING
     str_ptrs: Dict[int, int] = {}
     str_size = 0
 
-    argv_buf_ptr = NULL_POINTER_PADDING + STR_CAPACITY
+    argv_buf_ptr = SIM_NULL_POINTER_PADDING + SIM_STR_CAPACITY
     argc = 0
 
-    mem_buf_ptr  = NULL_POINTER_PADDING + STR_CAPACITY + ARGV_CAPACITY
+    mem_buf_ptr  = SIM_NULL_POINTER_PADDING + SIM_STR_CAPACITY + SIM_ARGV_CAPACITY
 
     fds: List[BinaryIO] = [sys.stdin.buffer, sys.stdout.buffer, sys.stderr.buffer]
 
@@ -140,12 +140,12 @@ def simulate_little_endian_linux(program: Program, argv: List[str]):
         mem[arg_ptr:arg_ptr+n] = value
         mem[arg_ptr+n] = 0
         str_size += n + 1
-        assert str_size <= STR_CAPACITY, "String buffer overflow"
+        assert str_size <= SIM_STR_CAPACITY, "String buffer overflow"
 
         argv_ptr = argv_buf_ptr+argc*8
         mem[argv_ptr:argv_ptr+8] = arg_ptr.to_bytes(8, byteorder='little')
         argc += 1
-        assert argc*8 <= ARGV_CAPACITY, "Argv buffer, overflow"
+        assert argc*8 <= SIM_ARGV_CAPACITY, "Argv buffer, overflow"
 
     ip = 0
     while ip < len(program):
@@ -166,7 +166,7 @@ def simulate_little_endian_linux(program: Program, argv: List[str]):
                     str_ptrs[ip] = str_ptr
                     mem[str_ptr:str_ptr+n] = value
                     str_size += n
-                    assert str_size <= STR_CAPACITY, "String buffer overflow"
+                    assert str_size <= SIM_STR_CAPACITY, "String buffer overflow"
                 stack.append(str_ptrs[ip])
                 ip += 1
             elif op.typ == OpType.IF:
