67e06a5c24aceb3aa8bab579a4241ef0de900825
rexim <reximkut@gmail.com>
2021-10-19 07:41:50 +0700

    No need to have a flag to customize memory capacity

--- porth.py
+++ porth.py
@@ -15,7 +15,6 @@ import traceback
 PORTH_EXT = '.porth'
 DEFAULT_EXPANSION_LIMIT=1000
 EXPANSION_DIAGNOSTIC_LIMIT=10
-# TODO: customize memory capacity via a flag
 SIM_NULL_POINTER_PADDING = 1 # just a little bit of a padding at the beginning of the memory to make 0 an invalid address
 SIM_STR_CAPACITY  = 640_000
 SIM_ARGV_CAPACITY = 640_000
