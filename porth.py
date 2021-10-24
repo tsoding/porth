7d297c1fa530757140649e25238bc3a5b8c545a5
rexim <reximkut@gmail.com>
2021-10-25 06:47:07 +0700

    [porth.porth]delete try_to_parse_word_as_int_or_fail_as_unknown_word

--- porth.py
+++ porth.py
@@ -15,7 +15,7 @@ import traceback
 PORTH_EXT = '.porth'
 DEFAULT_EXPANSION_LIMIT=1000
 EXPANSION_DIAGNOSTIC_LIMIT=10
-X86_64_RET_STACK_CAP=4096
+X86_64_RET_STACK_CAP=8192
 SIM_NULL_POINTER_PADDING = 1 # just a little bit of a padding at the beginning of the memory to make 0 an invalid address
 SIM_STR_CAPACITY  = 640_000
 SIM_ARGV_CAPACITY = 640_000
